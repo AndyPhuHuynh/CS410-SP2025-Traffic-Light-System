fe2c2e6b7cae63146d33595fad127c3f2719958f
NNL <n.nogueiralima001@umb.edu>
2025-03-27 13:05:56 -0400

    Traffic Light (Grey-Circles)

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,71 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const TrafficLightApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TrafficLightApp extends StatelessWidget {
+  const TrafficLightApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+    Widget build(BuildContext context) {
+      Widget lightRectangle = Container(
+        width: 400.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+          color: Colors.black,
+          shape: BoxShape.rectangle,
+        ),
+      );
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Traffic Lights',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
-      ),
+      color: Colors.green,
+      home: Scaffold(
+          body: Center(
+              child: Stack(
+                  children: <Widget>[
+                    lightRectangle,
+                    Positioned(
+                      top: 10.0,
+                      left: 10.0,
+                      child: Container (
+                          width: 100.0,
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            shape: BoxShape.circle,
+                          )
+                      ),
+                    ),
+                    Positioned(
+                      top: 10.0,
+                      left: 60.0,
+                      child: Container (
+                          width: 100.0,
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            shape: BoxShape.circle,
+                          )
+                      ),
+                    ),
+                    Positioned(
+                      top: 10.0,
+                      right: 110.0,
+                      child: Container (
+                          width: 100.0,
+                          height: 100.0,
+                          decoration: BoxDecoration(
+                            color: Colors.grey,
+                            shape: BoxShape.circle,
+                          )
+                      ),
+                    ),
+                  ]
+              )
+          )
+      )
     );
   }
 }
