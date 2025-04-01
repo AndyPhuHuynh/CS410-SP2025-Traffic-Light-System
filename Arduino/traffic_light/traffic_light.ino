479e97de6e8d349217c33ced7836da1b4048f307
AndyPhuHuynh <andyphuhuynh4@gmail.com>
2025-04-01 13:31:22 -0400

    Add timer

--- Arduino/traffic_light/traffic_light.ino
+++ Arduino/traffic_light/traffic_light.ino
@@ -1,6 +1,7 @@
 uint8_t input_pin = 22;
 uint8_t pir_pin = 19;
 uint8_t green_pin = 18;
+uint8_t yellow_pin = 4;
 uint8_t red_pin = 21;
 
 class TrafficLight {
@@ -31,6 +32,24 @@ public:
   void setRedLight(uint8_t mode) {
     digitalWrite(redPin, mode);
   }
+
+  void setOnlyGreen() {
+    setGreenLight(HIGH);
+    setYellowLight(LOW);
+    setRedLight(LOW);
+  }
+
+  void setOnlyYellow() {
+    setGreenLight(LOW);
+    setYellowLight(HIGH);
+    setRedLight(LOW);
+  }
+
+  void setOnlyRed() {
+    setGreenLight(LOW);
+    setYellowLight(LOW);
+    setRedLight(HIGH);
+  }
 };
 
 class Sensor {
@@ -49,7 +68,57 @@ public:
   } 
 };
 
-TrafficLight light(green_pin, 0, red_pin);
+class Timer {
+  unsigned long m_timerDuration = 0;
+  unsigned long m_prevTimeMilli = 0;
+  unsigned long m_timerMilli = 0;
+  bool m_countingDown = false;
+public:
+  Timer(unsigned long durationMilli) {
+    m_prevTimeMilli = millis();
+    m_timerDuration = durationMilli;
+  }
+
+  // Must be called at the beginning of the loop function before an timer logic
+  void update() {
+    Serial.println("countingdown\n");
+    if (m_countingDown) {
+      unsigned long deltaTMilli = millis() - m_prevTimeMilli;
+      m_prevTimeMilli = millis();
+      m_timerMilli += deltaTMilli;
+    }
+    if (m_timerMilli >= m_timerDuration) {
+      m_countingDown = false;
+    }
+  }
+
+  bool countDownEnded() {
+    return m_timerMilli >= m_timerDuration;
+  }
+
+  void start() {
+    m_prevTimeMilli = millis();
+    m_countingDown = true;
+  }
+
+  void resetCountDown() {
+    m_timerMilli = 0;
+  }
+
+  unsigned long getTimeMilli() {
+    return m_timerMilli;
+  }
+
+  double getTimeSeconds() {
+    return static_cast<double>(m_timerMilli) / 1000;
+  }
+};
+
+Timer *greenTimer;
+Timer *yellowTimer;
+Timer *redTimer;
+
+TrafficLight light(green_pin, yellow_pin, red_pin);
 Sensor lineSensor(input_pin);
 Sensor pirSensor(pir_pin);
 
@@ -59,13 +128,39 @@ void setup() {
   light.setupPins();
   lineSensor.setupPins();
   pirSensor.setupPins();
+
+  greenTimer = new Timer(2000);
+  yellowTimer = new Timer(1000);
+  redTimer = new Timer(2000);
+  
+  greenTimer->start();
+  light.setOnlyGreen();
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  light.setRedLight(lineSensor.read());
-  light.setGreenLight(pirSensor.read());
-  
-  delay(100);
+  greenTimer->update();
+  yellowTimer->update();
+  redTimer->update();
+
+  if (greenTimer->countDownEnded()) {
+    Serial.println("greenEnd");
+    light.setOnlyYellow();
+    yellowTimer->start();
+    greenTimer->resetCountDown();
+  }
+
+  if (yellowTimer->countDownEnded()) {
+    Serial.println("yellowEnd");
+    light.setOnlyRed();
+    redTimer->start();
+    yellowTimer->resetCountDown();
+  }
+
+  if (redTimer->countDownEnded()) {
+    Serial.println("redEnd");
+    light.setOnlyGreen();
+    greenTimer->start();
+    redTimer->resetCountDown();
+  }
 }
