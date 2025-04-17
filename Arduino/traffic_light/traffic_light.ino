6765f8187250f670cdf7d13488bedbb02b29cfc4
nnogueiralima <103339706+nnogueiralima@users.noreply.github.com>
2025-04-17 14:58:57 -0400

    Update traffic_light.ino

--- Arduino/traffic_light/traffic_light.ino
+++ Arduino/traffic_light/traffic_light.ino
@@ -1,13 +1,7 @@
 #include "Devices.h"
-#include "Intersection.h"
 
-// Line sensor pins
-uint8_t line_pin_1 = 22;
-uint8_t line_pin_2 = 23;
-
-// Pir sensor pins
-uint8_t pir_pin_1 = 27;
-uint8_t pir_pin_2 = 26;
+uint8_t input_pin = 22;
+uint8_t pir_pin = 30;
 
 // Light 1 pins
 uint8_t green_pin_1 = 17;
@@ -19,22 +13,18 @@ uint8_t green_pin_2 = 21;
 uint8_t yellow_pin_2 = 19;
 uint8_t red_pin_2 = 18;
 
-Intersection *intersection;
+TrafficLight *light1;
+TrafficLight *light2;
+
+Sensor *pir1;
 
 void setup() {
     Serial.begin(9600);
-    TrafficLight light1 = TrafficLight(green_pin_1, yellow_pin_1, red_pin_1);
-    TrafficLight light2 = TrafficLight(green_pin_2, yellow_pin_2, red_pin_2);
-
-    Sensor line1 = Sensor(line_pin_1);
-    Sensor line2 = Sensor(line_pin_2);
-
-    Sensor pir1 = Sensor(pir_pin_1);
-    Sensor pir2 = Sensor(pir_pin_2);
-
-    intersection = new Intersection(light1, light2, line1, line2, pir1, pir2); 
+    light1 = new TrafficLight(green_pin_1, yellow_pin_1, red_pin_1, State::ConstantGreen);
+    light2 = new TrafficLight(green_pin_2, yellow_pin_2, red_pin_2, State::GreenToRed);
 }
 
 void loop() {
-    intersection->update();
+    light1->update();
+    light2->update();
 }
